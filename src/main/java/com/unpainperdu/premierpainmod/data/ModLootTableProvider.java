af645ab7ab528e6fc42c13b480edfc86a2910952
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-05-13 10:57:37 +0200

    fix : statue loot twice when destroyed

--- src/main/java/com/unpainperdu/premierpainmod/data/ModLootTableProvider.java
+++ src/main/java/com/unpainperdu/premierpainmod/data/ModLootTableProvider.java
@@ -3,12 +3,17 @@ package com.unpainperdu.premierpainmod.data;
 
 import com.unpainperdu.premierpainmod.PremierPainMod;
 import com.unpainperdu.premierpainmod.util.register.BlockRegister;
+import com.unpainperdu.premierpainmod.world.block.VillagerStatue;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.loot.BlockLootSubProvider;
 import net.minecraft.data.loot.LootTableProvider;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DoorBlock;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
+import net.minecraft.world.level.storage.loot.LootTable;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.minecraft.core.registries.BuiltInRegistries;
 import org.jetbrains.annotations.NotNull;
@@ -40,8 +45,8 @@ public class ModLootTableProvider extends LootTableProvider
         @Override
         public void generate()
         {
-            this.dropSelf(BlockRegister.OAK_VILLAGER_STATUE.get());
-            this.dropSelf(BlockRegister.BIRCH_VILLAGER_STATUE.get());
+            super.add(BlockRegister.OAK_VILLAGER_STATUE.get(), createDoubleBlockTable(BlockRegister.OAK_VILLAGER_STATUE.get()));
+            super.add(BlockRegister.BIRCH_VILLAGER_STATUE.get(), createDoubleBlockTable(BlockRegister.BIRCH_VILLAGER_STATUE.get()));
         }
         @Override
         protected @NotNull Iterable<Block> getKnownBlocks()
@@ -50,6 +55,10 @@ public class ModLootTableProvider extends LootTableProvider
                     .filter(e -> e.getKey().location().getNamespace().equals(PremierPainMod.MODID))
                     .map(Map.Entry::getValue)
                     .collect(Collectors.toList());
+        }
+        private LootTable.Builder createDoubleBlockTable(Block statue)
+        {
+            return this.createSinglePropConditionTable(statue, VillagerStatue.HALF, DoubleBlockHalf.LOWER);
         }
     }
 }
