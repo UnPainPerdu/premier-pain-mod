acd1c71fae3fc2137910eecdab5c06b622817e12
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-05-13 15:42:46 +0200

    remake : simplifie statue recipe generation

--- src/main/java/com/unpainperdu/premierpainmod/data/ModRecipeProvider.java
+++ src/main/java/com/unpainperdu/premierpainmod/data/ModRecipeProvider.java
@@ -1,6 +1,7 @@
 package com.unpainperdu.premierpainmod.data;
 
 import com.unpainperdu.premierpainmod.util.register.BlockRegister;
+import net.minecraft.advancements.Criterion;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.recipes.RecipeCategory;
@@ -8,6 +9,7 @@ import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ItemLike;
 import net.minecraft.world.level.block.Blocks;
 import net.neoforged.neoforge.common.conditions.IConditionBuilder;
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +18,7 @@ import java.util.concurrent.CompletableFuture;
 
 public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder
 {
+    private static RecipeOutput recipeOutput;
     public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider)
     {
         super(output, lookupProvider);
@@ -24,26 +27,19 @@ public class ModRecipeProvider extends RecipeProvider implements IConditionBuild
     @Override
     protected void buildRecipes(@NotNull RecipeOutput pRecipeOutput)
     {
-        ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, new ItemStack(BlockRegister.OAK_VILLAGER_STATUE, 1))
-                .define('#', Blocks.OAK_PLANKS)
+        ModRecipeProvider.recipeOutput = pRecipeOutput;
+        statueRecipeBuilder(BlockRegister.OAK_VILLAGER_STATUE,"has_oak_planks",Blocks.OAK_PLANKS);
+        statueRecipeBuilder(BlockRegister.BIRCH_VILLAGER_STATUE,"has_birch_planks",Blocks.BIRCH_PLANKS);
+        statueRecipeBuilder(BlockRegister.STONE_VILLAGER_STATUE,"has_stone",Blocks.STONE);
+    }
+    private void statueRecipeBuilder(ItemLike craftedBlock,String pName, ItemLike ingredient)
+    {
+        ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, new ItemStack(craftedBlock, 1))
+                .define('#', ingredient)
                 .pattern(" # ")
-                .pattern("###")
                 .pattern(" # ")
-                .unlockedBy("has_oak_planks", has(Blocks.OAK_PLANKS))
-                .save(pRecipeOutput);
-        ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, new ItemStack(BlockRegister.BIRCH_VILLAGER_STATUE, 1))
-                .define('#', Blocks.BIRCH_PLANKS)
                 .pattern(" # ")
-                .pattern("###")
-                .pattern(" # ")
-                .unlockedBy("has_birch_planks", has(Blocks.BIRCH_PLANKS))
-                .save(pRecipeOutput);
-        ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, new ItemStack(BlockRegister.STONE_VILLAGER_STATUE, 1))
-                .define('#', Blocks.STONE)
-                .pattern(" # ")
-                .pattern("###")
-                .pattern(" # ")
-                .unlockedBy("has_stone_planks", has(Blocks.STONE))
-                .save(pRecipeOutput);
+                .unlockedBy(pName, has(ingredient))
+                .save(ModRecipeProvider.recipeOutput);
     }
 }
