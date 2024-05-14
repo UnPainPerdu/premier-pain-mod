15a43a8123cf56c6357d581c8407824b0b41d28e
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-05-14 17:44:27 +0200

    fix : name fix in code

--- src/main/java/com/unpainperdu/premierpainmod/util/register/CreativeTabRegister.java
+++ src/main/java/com/unpainperdu/premierpainmod/util/register/CreativeTabRegister.java
@@ -15,7 +15,7 @@ public class CreativeTabRegister
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, PremierPainMod.MODID);
 
     // Creates a creative tab with the id "premierpaindmod:premier_pain_mod" for the example item, that is placed after the combat tab
-    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("premier_pain_mod", () -> CreativeModeTab.builder()
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> PREMIER_PAIN_MOD = CREATIVE_MODE_TABS.register("premier_pain_mod", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.premierpainmod")) //The language key for the title of your CreativeModeTab
             .withTabsBefore(CreativeModeTabs.COMBAT)
             .icon(() -> ItemRegister.VILLAGER_ICON.get().getDefaultInstance())
