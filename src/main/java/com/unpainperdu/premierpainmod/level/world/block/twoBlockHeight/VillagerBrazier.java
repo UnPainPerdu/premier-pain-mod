d294f1f050228fdbcb829aecbe756626b182c146
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-06-03 13:15:02 +0200

    fix : brazier does dammage on step when lit

--- src/main/java/com/unpainperdu/premierpainmod/level/world/block/twoBlockHeight/VillagerBrazier.java
+++ src/main/java/com/unpainperdu/premierpainmod/level/world/block/twoBlockHeight/VillagerBrazier.java
@@ -11,6 +11,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.projectile.Projectile;
@@ -18,6 +19,7 @@ import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.FlintAndSteelItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
@@ -33,7 +35,6 @@ import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nullable;
-import java.util.Optional;
 
 import static java.lang.Boolean.FALSE;
 import static java.lang.Boolean.TRUE;
@@ -231,5 +232,16 @@ public class VillagerBrazier extends AbstactTwoBlockHeightBlock
                 }
             }
         }
+    }
+    @Override
+    public void stepOn(Level pLevel, BlockPos pPos, BlockState pState, Entity pEntity)
+    {
+        if(pState.getValue(LIT))
+        {
+            if (!pEntity.isSteppingCarefully() && pEntity instanceof LivingEntity && !EnchantmentHelper.hasFrostWalker((LivingEntity) pEntity)) {
+                pEntity.hurt(pLevel.damageSources().hotFloor(), 1.0F);
+            }
+        }
+        super.stepOn(pLevel, pPos, pState, pEntity);
     }
 }
