644bbd949ab970926f481961eda7829fae2b3061
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-06-06 22:43:45 +0200

    add : color property is settable by rightclicking on the table with the right item

--- src/main/java/com/unpainperdu/premierpainmod/level/world/block/VillagerTableBlock.java
+++ src/main/java/com/unpainperdu/premierpainmod/level/world/block/VillagerTableBlock.java
@@ -5,25 +5,42 @@ import com.unpainperdu.premierpainmod.level.world.block.state.properties.Village
 import com.unpainperdu.premierpainmod.level.world.block.state.properties.VillagerWorkshopPart;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.ItemInteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.BucketItem;
+import net.minecraft.world.item.FlintAndSteelItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.ShearsItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.PipeBlock;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
+import net.minecraft.world.level.block.WoolCarpetBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.block.state.properties.DoubleBlockHalf;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
 import java.util.Map;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+import static net.minecraft.world.level.material.Fluids.WATER;
 
 public class VillagerTableBlock extends Block implements SimpleWaterloggedBlock
 {
@@ -118,5 +135,61 @@ public class VillagerTableBlock extends Block implements SimpleWaterloggedBlock
     static
     {
         COLOR = EnumProperty.create("villagertablecarpetcolor", VillagerTableCarpetColor.class);
+    }
+
+    @Override
+    protected ItemInteractionResult useItemOn(
+            ItemStack pStack, BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHitResult
+    )
+    {
+        if (pLevel.isClientSide)
+        {
+            return ItemInteractionResult.SUCCESS;
+        }
+        else
+        {
+            Block carpetBlock = Block.byItem(pStack.getItem());
+            if(carpetBlock instanceof WoolCarpetBlock)
+            {
+
+                pLevel.setBlock(pPos, pState.setValue(COLOR, colorDedection((WoolCarpetBlock) carpetBlock)), 3);
+                return ItemInteractionResult.CONSUME;
+            }
+
+            if ((pStack.getItem() instanceof ShearsItem) && (pState.getValue(COLOR) != VillagerTableCarpetColor.NONE))
+            {
+                pLevel.setBlock(pPos, pState.setValue(COLOR, VillagerTableCarpetColor.NONE), 3);
+                pLevel.playSound(null, pPos, SoundEvents.SHEEP_SHEAR, SoundSource.BLOCKS, 1.0F, 1.0F);
+                return ItemInteractionResult.CONSUME;
+            }
+            else
+            {
+                return ItemInteractionResult.SUCCESS;
+            }
+
+        }
+    }
+    private VillagerTableCarpetColor colorDedection(WoolCarpetBlock carpetBlock)
+    {
+        switch (carpetBlock.getColor())
+        {
+            case WHITE : return VillagerTableCarpetColor.WHITE;
+            case LIGHT_GRAY : return VillagerTableCarpetColor.LIGHT_GRAY;
+            case GRAY : return VillagerTableCarpetColor.GRAY;
+            case BLACK : return VillagerTableCarpetColor.BLACK;
+            case BROWN : return VillagerTableCarpetColor.BROWN;
+            case RED : return VillagerTableCarpetColor.RED;
+            case ORANGE : return VillagerTableCarpetColor.ORANGE;
+            case YELLOW : return VillagerTableCarpetColor.YELLOW;
+            case LIME : return VillagerTableCarpetColor.LIME;
+            case GREEN : return VillagerTableCarpetColor.GREEN;
+            case CYAN : return VillagerTableCarpetColor.CYAN;
+            case LIGHT_BLUE : return VillagerTableCarpetColor.LIGHT_BLUE;
+            case BLUE : return VillagerTableCarpetColor.BLUE;
+            case PURPLE : return VillagerTableCarpetColor.PURPLE;
+            case MAGENTA : return VillagerTableCarpetColor.MAGENTA;
+            case PINK : return VillagerTableCarpetColor.PINK;
+            default: return VillagerTableCarpetColor.NONE;
+        }
     }
 }
