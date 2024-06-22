09b8b6cfaf53b0835d776588cd4cea4d82cf0db0
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-06-22 10:25:15 +0200

    fix : render of item in Villager pedestal is fine now

--- src/main/java/com/unpainperdu/premierpainmod/client/render/renderBlockEntity/VillagerPedestalRender.java
+++ src/main/java/com/unpainperdu/premierpainmod/client/render/renderBlockEntity/VillagerPedestalRender.java
@@ -9,9 +9,9 @@ import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.core.Direction;
 import net.minecraft.core.NonNullList;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.item.*;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 
 public class VillagerPedestalRender implements BlockEntityRenderer<PedestalBlockEntity>
 {
@@ -21,24 +21,63 @@ public class VillagerPedestalRender implements BlockEntityRenderer<PedestalBlock
     {
         Direction direction = pBlockEntity.getBlockState().getValue(VillagerPedestalBlock.FACING);
         NonNullList<ItemStack> nonnulllist = pBlockEntity.getItems();
+        ItemStack itemstack = nonnulllist.getFirst();
+        Item item = itemstack.getItem();
+        Block block = Block.byItem(item);
         int i = (int)pBlockEntity.getBlockPos().asLong();
-
-        for (int j = 0; j < nonnulllist.size(); j++)
+        if (itemstack != ItemStack.EMPTY)
         {
-            ItemStack itemstack = nonnulllist.get(j);
-            if (itemstack != ItemStack.EMPTY)
+            if(block != Blocks.AIR)
             {
                 pPoseStack.pushPose();
-                pPoseStack.translate(0.5F, 0.44921875F, 0.5F);
-                Direction direction1 = Direction.from2DDataValue((j + direction.get2DDataValue()) % 4);
+                pPoseStack.translate(0.5F, 1.25F, 0.5F);
+                Direction direction1 = Direction.from2DDataValue((direction.get2DDataValue()) % 4);
                 float f = -direction1.toYRot();
                 pPoseStack.mulPose(Axis.YP.rotationDegrees(f));
-                pPoseStack.mulPose(Axis.XP.rotationDegrees(90.0F));
-                pPoseStack.translate(-0.3125F, -0.3125F, 0.0F);
-                pPoseStack.scale(0.375F, 0.375F, 0.375F);
-                Minecraft.getInstance().getItemRenderer().renderStatic(itemstack, ItemDisplayContext.FIXED, pPackedLight, pPackedOverlay, pPoseStack, pBuffer, pBlockEntity.getLevel(), i + j);
+                pPoseStack.mulPose(Axis.XP.rotationDegrees(0F));
+                pPoseStack.scale(1F, 1F, 1F);
+                Minecraft.getInstance().getItemRenderer().renderStatic(itemstack, ItemDisplayContext.FIXED, pPackedLight, pPackedOverlay, pPoseStack, pBuffer, pBlockEntity.getLevel(), i);
                 pPoseStack.popPose();
             }
+            else if((item instanceof SwordItem)
+                    || (item instanceof PickaxeItem)
+                    || (item instanceof ShovelItem)
+                    || (item instanceof AxeItem)
+                    || (item instanceof HoeItem)
+                    || (item instanceof BowItem)
+                    || (item instanceof CrossbowItem)
+                    || (item instanceof ShieldItem)
+                    || (item instanceof MaceItem)
+                    || (item instanceof TridentItem)
+                    || (item instanceof ArmorItem)
+            )
+            {
+                pPoseStack.pushPose();
+                pPoseStack.translate(0.5F, 1.5F, 0.5F);
+                Direction direction1 = Direction.from2DDataValue((direction.get2DDataValue()) % 4);
+                float f = -direction1.toYRot();
+                pPoseStack.mulPose(Axis.YP.rotationDegrees(f));
+                if(item instanceof SwordItem)
+                {
+                    pPoseStack.mulPose(Axis.ZP.rotationDegrees(135F));
+                }
+                pPoseStack.scale(0.85F, 0.85F, 0.85F);
+                Minecraft.getInstance().getItemRenderer().renderStatic(itemstack, ItemDisplayContext.FIXED, pPackedLight, pPackedOverlay, pPoseStack, pBuffer, pBlockEntity.getLevel(), i);
+                pPoseStack.popPose();
+            }
+            else
+            {
+                pPoseStack.pushPose();
+                pPoseStack.translate(0.5F, 1.25F, 0.5F);
+                Direction direction1 = Direction.from2DDataValue((direction.get2DDataValue()) % 4);
+                float f = -direction1.toYRot();
+                pPoseStack.mulPose(Axis.YP.rotationDegrees(f));
+                pPoseStack.mulPose(Axis.XP.rotationDegrees(0F));
+                pPoseStack.scale(0.5F, 0.5F, 0.5F);
+                Minecraft.getInstance().getItemRenderer().renderStatic(itemstack, ItemDisplayContext.FIXED, pPackedLight, pPackedOverlay, pPoseStack, pBuffer, pBlockEntity.getLevel(), i);
+                pPoseStack.popPose();
+            }
+
         }
     }
 }
