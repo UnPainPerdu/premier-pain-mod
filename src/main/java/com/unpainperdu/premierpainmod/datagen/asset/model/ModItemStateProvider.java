e76831babd7e92f783277cb73fb6ff4327129d66
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-07-29 11:15:19 +0200

    add : tesxture for all villager singing stone

--- src/main/java/com/unpainperdu/premierpainmod/datagen/asset/model/ModItemStateProvider.java
+++ src/main/java/com/unpainperdu/premierpainmod/datagen/asset/model/ModItemStateProvider.java
@@ -1,28 +1,15 @@
 package com.unpainperdu.premierpainmod.datagen.asset.model;
 
 import com.unpainperdu.premierpainmod.PremierPainMod;
-import com.unpainperdu.premierpainmod.level.world.block.VillagerChairBlock;
-import com.unpainperdu.premierpainmod.level.world.block.VillagerPedestalBlock;
-import com.unpainperdu.premierpainmod.level.world.block.VillagerTableBlock;
-import com.unpainperdu.premierpainmod.level.world.block.twoBlockHeight.VillagerBrazier;
-import com.unpainperdu.premierpainmod.level.world.block.twoBlockHeight.VillagerStatue;
-import com.unpainperdu.premierpainmod.level.world.block.twoBlockHeight.VillagerThroneChairBlock;
-import com.unpainperdu.premierpainmod.level.world.block.twoBlockWidth.VillagerWorkshop;
-import com.unpainperdu.premierpainmod.level.world.block.twoBlockWidthWithBlockEntity.VillagerDrawer;
-import com.unpainperdu.premierpainmod.level.world.block.twoBlockWidthWithBlockEntity.villagerShelf.StandingVillagerShelf;
-import com.unpainperdu.premierpainmod.level.world.block.twoBlockWidthWithBlockEntity.villagerShelf.VillagerShelf;
-import com.unpainperdu.premierpainmod.level.world.block.twoBlockWidthWithBlockEntity.villagerShelf.WallVillagerShelf;
 import com.unpainperdu.premierpainmod.level.world.item.items.VillagerShelfItem;
-import com.unpainperdu.premierpainmod.util.register.BlockList;
+import com.unpainperdu.premierpainmod.level.world.item.items.VillagerSingingStone;
 import com.unpainperdu.premierpainmod.util.register.ItemList;
 import com.unpainperdu.premierpainmod.util.register.ItemRegister;
 import net.minecraft.data.PackOutput;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
-import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredItem;
 
 public class ModItemStateProvider extends ItemModelProvider
@@ -40,11 +27,12 @@ public class ModItemStateProvider extends ItemModelProvider
         {
             Item item = deferredItem.get();
             if(item instanceof VillagerShelfItem) {villagerShelfItem(item);}
+            if(item instanceof VillagerSingingStone){villagerSingingStone(item);}
         }
     }
     private void item(Item item)
     {
-        String name = BuiltInRegistries.ITEM.getKey(item).toString().replace(PremierPainMod.MODID+":","");
+        String name = getName(item);
         getBuilder(name)
                 .parent(getExistingFile(mcLoc("item/generated")))
                 .texture("layer0","item/" + name);
@@ -52,9 +40,21 @@ public class ModItemStateProvider extends ItemModelProvider
 
     private void villagerShelfItem(Item item)
     {
-        String name = BuiltInRegistries.ITEM.getKey(item).toString().replace(PremierPainMod.MODID+":","");
+        String name = getName(item);
         getBuilder(name)
                 .parent(getExistingFile(mcLoc("item/generated")))
                 .texture("layer0","item/villager_shelf/" + name);
+    }
+
+    private void villagerSingingStone(Item item)
+    {
+        String name = getName(item);
+        getBuilder(name)
+                .parent(getExistingFile(mcLoc("item/generated")))
+                .texture("layer0","item/villager_singing_stone/" + name);
+    }
+    private String getName(Item item)
+    {
+        return BuiltInRegistries.ITEM.getKey(item).toString().replace(PremierPainMod.MODID+":","");
     }
 }
