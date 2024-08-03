29da8779c620f76a200a61c7ccfa42506b3baf69
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-08-03 15:06:34 +0200

    remake : voxelshape of villager throne shair

--- src/main/java/com/unpainperdu/premierpainmod/level/world/block/twoBlockHeight/VillagerThroneChairBlock.java
+++ src/main/java/com/unpainperdu/premierpainmod/level/world/block/twoBlockHeight/VillagerThroneChairBlock.java
@@ -39,27 +39,38 @@ public class VillagerThroneChairBlock extends AbstactTwoBlockHeightBlock
         return CODEC;
     }
 
-    private static final VoxelShape COMMON_SHAPE = Block.box(1, 0, 1, 15, 8, 15);
-
-    private static final VoxelShape NORTH_UPPER_SHAPE = Block.box(3, 0, 1, 13, 12, 3);
-    private static final VoxelShape NORTH_BELOW_SHAPE =Shapes.or(COMMON_SHAPE, Block.box(3, 8, 1, 13, 16, 3));
-
-    private static final VoxelShape EAST_UPPER_SHAPE = Block.box(13, 0, 3, 15, 12, 13);
-    private static final VoxelShape EAST_BELOW_SHAPE =Shapes.or(COMMON_SHAPE, Block.box(13, 8, 3, 15, 16, 13));
-
-    private static final VoxelShape SOUTH_UPPER_SHAPE = Block.box(3, 0, 13, 13, 12, 15);
-    private static final VoxelShape SOUTH_BELOW_SHAPE =Shapes.or(COMMON_SHAPE, Block.box(3, 8, 13, 13, 16, 15));
-
-    private static final VoxelShape WEAST_UPPER_SHAPE = Block.box(1, 0, 3, 3, 12, 13);
-    private static final VoxelShape WEST_BELOW_SHAPE =Shapes.or(COMMON_SHAPE, Block.box(1, 8, 3, 3, 16, 13));
-
     @Override
     public VoxelShape getShape(BlockState pState, BlockGetter p_60556_, BlockPos bPos, CollisionContext p_60558_)
     {
+        VoxelShape COMMON_SHAPE = Block.box(3, 1, 3, 13, 8, 13);
+
+        VoxelShape NORTH_UPPER_SHAPE = Block.box(3, 0, 1, 13, 12, 3);
+        VoxelShape NORTH_BELOW_SHAPE =Shapes.or(COMMON_SHAPE, Block.box(3, 8, 1, 13, 16, 3));
+
+        VoxelShape EAST_UPPER_SHAPE = Block.box(13, 0, 3, 15, 12, 13);
+        VoxelShape EAST_BELOW_SHAPE =Shapes.or(COMMON_SHAPE, Block.box(13, 8, 3, 15, 16, 13));
+
+        VoxelShape SOUTH_UPPER_SHAPE = Block.box(3, 0, 13, 13, 12, 15);
+        VoxelShape SOUTH_BELOW_SHAPE =Shapes.or(COMMON_SHAPE, Block.box(3, 8, 13, 13, 16, 15));
+
+        VoxelShape WEAST_UPPER_SHAPE = Block.box(1, 0, 3, 3, 12, 13);
+        VoxelShape WEST_BELOW_SHAPE =Shapes.or(COMMON_SHAPE, Block.box(1, 8, 3, 3, 16, 13));
+
+        VoxelShape NW_FOOT_SHAPE = Block.box(1,0,1,3,12,3);
+        VoxelShape NE_FOOT_SHAPE = Block.box(13,0,1,15,12,3);
+        VoxelShape SW_FOOT_SHAPE = Block.box(1,0,13,3,12,15);
+        VoxelShape SE_FOOT_SHAPE = Block.box(13,0,13,15,12,15);
+
+        VoxelShape NS_SIDE_SHAPE = Shapes.or(Block.box(1,10,1,3,12,15), Block.box(13,10,1,15,12,15));
+        VoxelShape WE_SIDE_SHAPE = Shapes.or(Block.box(1,10,1,15,12,3), Block.box(1,10,13,15,12,15));
+
+        VoxelShape FEETS = Shapes.or(NW_FOOT_SHAPE,NE_FOOT_SHAPE,SW_FOOT_SHAPE,SE_FOOT_SHAPE);
+
         DoubleBlockHalf doubleblockhalf = pState.getValue(HALF);
         Direction direction = pState.getValue(FACING);
         switch (direction)
         {
+
             case Direction.EAST:
             {
                 if (doubleblockhalf == DoubleBlockHalf.UPPER)
@@ -68,7 +79,7 @@ public class VillagerThroneChairBlock extends AbstactTwoBlockHeightBlock
                 }
                 else
                 {
-                    return EAST_BELOW_SHAPE;
+                    return Shapes.or(EAST_BELOW_SHAPE, FEETS, WE_SIDE_SHAPE);
                 }
             }
             case Direction.SOUTH:
@@ -79,7 +90,7 @@ public class VillagerThroneChairBlock extends AbstactTwoBlockHeightBlock
                 }
                 else
                 {
-                    return SOUTH_BELOW_SHAPE;
+                    return Shapes.or(SOUTH_BELOW_SHAPE, FEETS, NS_SIDE_SHAPE);
                 }
             }
             case Direction.WEST:
@@ -90,7 +101,7 @@ public class VillagerThroneChairBlock extends AbstactTwoBlockHeightBlock
                 }
                 else
                 {
-                    return WEST_BELOW_SHAPE;
+                    return Shapes.or(WEST_BELOW_SHAPE, FEETS, WE_SIDE_SHAPE);
                 }
             }
             default:
@@ -101,7 +112,7 @@ public class VillagerThroneChairBlock extends AbstactTwoBlockHeightBlock
                 }
                 else
                 {
-                    return NORTH_BELOW_SHAPE;
+                    return Shapes.or(NORTH_BELOW_SHAPE, FEETS, NS_SIDE_SHAPE);
                 }
             }
         }
