e00f840de41378120d0ad0b7e92f37002409f875
Ethan Capouillez <ethan.capouillez@hotmail.be>
2024-10-23 22:57:44 +0200

    remake : change icon and add a research bar

--- src/main/java/com/unpainperdu/premierpainmod/util/register/creativeTab/CreativeTabRegister.java
+++ src/main/java/com/unpainperdu/premierpainmod/util/register/creativeTab/CreativeTabRegister.java
@@ -1,9 +1,11 @@
 package com.unpainperdu.premierpainmod.util.register.creativeTab;
 
 import com.unpainperdu.premierpainmod.PremierPainMod;
+import com.unpainperdu.premierpainmod.util.register.BlockRegister;
 import com.unpainperdu.premierpainmod.util.register.ItemRegister;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.neoforged.bus.api.IEventBus;
@@ -17,7 +19,9 @@ public class CreativeTabRegister
     public static final DeferredHolder<CreativeModeTab, CreativeModeTab> PREMIER_PAIN_MOD = CREATIVE_MODE_TABS.register("premier_pain_mod", () -> CreativeModeTab.builder()
             .title(Component.translatable("itemGroup.premierpainmod"))
             .withTabsBefore(CreativeModeTabs.COMBAT)
-            .icon(() -> ItemRegister.VILLAGER_ICON.get().getDefaultInstance())
+            .icon(() -> BlockRegister.VILLAGER_WORKSHOP.get().asItem().getDefaultInstance())
+            .withSearchBar(75)
+            .backgroundTexture(ResourceLocation.fromNamespaceAndPath(PremierPainMod.MOD_ID, "textures/gui/creative_tab/main_tab.png"))
             .displayItems(CreativeMainTab::generateCreativeMainTab).build());
 
     public static void register(IEventBus modEventBus)
