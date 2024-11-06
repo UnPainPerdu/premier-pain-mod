dd32481c46bee957d139727ba1300cf92f4e8fdb
UnPainPerdu <ethan.capouillez@hotmail.be>
2024-11-06 10:56:47 +0100

    feat : tags for biome (not tested and finished)

--- src/main/java/com/unpainperdu/premierpainmod/datagen/data/tag/ModBiomeTagProvider.java
+++ src/main/java/com/unpainperdu/premierpainmod/datagen/data/tag/ModBiomeTagProvider.java
@@ -1,9 +1,11 @@
 package com.unpainperdu.premierpainmod.datagen.data.tag;
 
 import com.unpainperdu.premierpainmod.PremierPainMod;
+import com.unpainperdu.premierpainmod.level.world.worldgen.biome.ModBiomes;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.BiomeTagsProvider;
+import net.minecraft.tags.BiomeTags;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
 
@@ -19,6 +21,19 @@ public class ModBiomeTagProvider extends BiomeTagsProvider
     @Override
     protected void addTags(HolderLookup.Provider pProvider)
     {
+
+        //Premier Pain ruins
+            //forest
+        this.tag(BiomeTags.IS_FOREST).add(ModBiomes.FOREST_PREMIER_PAIN_RUINS);
+        this.tag(BiomeTags.IS_OVERWORLD).add(ModBiomes.FOREST_PREMIER_PAIN_RUINS);
+            //swamp
+        this.tag(BiomeTags.ALLOWS_SURFACE_SLIME_SPAWNS).add(ModBiomes.SWAMP_PREMIER_PAIN_RUINS);
+        this.tag(BiomeTags.IS_OVERWORLD).add(ModBiomes.SWAMP_PREMIER_PAIN_RUINS);
+            //desert
+        this.tag(BiomeTags.SNOW_GOLEM_MELTS).add(ModBiomes.SAND_DESERT_PREMIER_PAIN_RUINS);
+        this.tag(BiomeTags.SPAWNS_WARM_VARIANT_FROGS).add(ModBiomes.SAND_DESERT_PREMIER_PAIN_RUINS);
+        this.tag(BiomeTags.IS_OVERWORLD).add(ModBiomes.SAND_DESERT_PREMIER_PAIN_RUINS);
+
         super.addTags(pProvider);
     }
 }
