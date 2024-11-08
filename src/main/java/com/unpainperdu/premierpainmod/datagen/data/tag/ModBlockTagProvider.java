78254c216a41428eb35047fb94ca3e2712f01db0
UnPainPerdu <ethan.capouillez@hotmail.be>
2024-11-08 10:43:37 +0100

    refactor : Block tag (not finished)

--- src/main/java/com/unpainperdu/premierpainmod/datagen/data/tag/ModBlockTagProvider.java
+++ src/main/java/com/unpainperdu/premierpainmod/datagen/data/tag/ModBlockTagProvider.java
@@ -32,84 +32,16 @@ public class ModBlockTagProvider extends BlockTagsProvider
     @Override
     protected void addTags(HolderLookup.@NotNull Provider pProvider)
     {
+        for(Block block : ModList.getAllMaterialsBlocks())
+        {
+            String blockName = BuiltInRegistries.BLOCK.getKey(block).toString().replace(PremierPainMod.MOD_ID + ":", "");
+            addTagForAllMaterialsBlock(block, blockName);
+        }
         for (DeferredBlock<Block> Defferedblock : ModList.ALL_BLOCKS)
         {
             Block block = Defferedblock.get();
             String blockName = BuiltInRegistries.BLOCK.getKey(block).toString().replace(PremierPainMod.MOD_ID + ":", "");
-            //mineable with pickaxe
-            if ((block instanceof VillagerWorkshop)
-                    || (blockName.contains("red_sandstone"))
-                    || (blockName.contains("sandstone"))
-                    || (blockName.contains("mossy_stone"))
-                    || (blockName.contains("end_stone"))
-                    || (blockName.contains("blackstone"))
-                    || (blockName.contains("redstone"))
-                    || (blockName.contains("dripstone"))
-                    || (blockName.contains("stone"))
-                    || (blockName.contains("andesite"))
-                    || (blockName.contains("diorite"))
-                    || (blockName.contains("granite"))
-                    || (blockName.contains("prismarine"))
-                    || (blockName.contains("purpur"))
-                    || (blockName.contains("deepslate"))
-                    || (blockName.contains("tuff"))
-                    || (blockName.contains("packed_mud"))
-                    || (blockName.contains("quartz"))
-                    || (blockName.contains("nether_bricks"))
-                    || (blockName.contains("basalt"))
-                    || (blockName.contains("coal"))
-                    || (blockName.contains("iron"))
-                    || (blockName.contains("gold"))
-                    || (blockName.contains("emerald"))
-                    || (blockName.contains("diamond"))
-                    || (blockName.contains("copper"))
-                    || (blockName.contains("lapis"))
-                    || (blockName.contains("netherite"))
-                    || (blockName.contains("obsidian"))
-                    || (blockName.contains("amethyst"))
-            )
-            {
-                this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block);
-                //need stone tool
-                if ((blockName.contains("iron"))
-                        || (blockName.contains("lapis"))
-                        || (blockName.contains("copper"))
-                )
-                {
-                    this.tag(BlockTags.NEEDS_STONE_TOOL).add(block);
-                }
-                //need iron tool
-                else if ((blockName.contains("gold"))
-                    || (blockName.contains("emerald"))
-                    || (blockName.contains("diamond"))
-                )
-                {
-                    this.tag(BlockTags.NEEDS_IRON_TOOL).add(block);
-                }
-                //need diamond tool
-                else if ((blockName.contains("netherite"))
-                        || (blockName.contains("obsidian"))
-                )
-                {
-                    this.tag(BlockTags.NEEDS_DIAMOND_TOOL).add(block);
-                }
-            }
-            //mineable with axe
-            else if ((blockName.contains("dark_oak"))
-                    || (blockName.contains("oak"))
-                    || (blockName.contains("birch"))
-                    || (blockName.contains("spruce"))
-                    || (blockName.contains("jungle"))
-                    || (blockName.contains("acacia"))
-                    || (blockName.contains("mangrove"))
-                    || (blockName.contains("cherry"))
-                    || (blockName.contains("crimson"))
-                    || (blockName.contains("warped"))
-                    || (blockName.contains("bamboo"))
-            )
-            {
-                this.tag(BlockTags.MINEABLE_WITH_AXE).add(block);
-            }
+
             //for all leaves
             if (block instanceof ModLeavesBlock)
             {
@@ -166,6 +98,84 @@ public class ModBlockTagProvider extends BlockTagsProvider
             {
                 this.tag(BlockTags.ENDERMAN_HOLDABLE).add(block);
             }
+        }
+    }
+
+    private void addTagForAllMaterialsBlock(Block block, String blockName)
+    {
+        //mineable with pickaxe
+        if ((block instanceof VillagerWorkshop)
+                || (blockName.contains("red_sandstone"))
+                || (blockName.contains("sandstone"))
+                || (blockName.contains("mossy_stone"))
+                || (blockName.contains("end_stone"))
+                || (blockName.contains("blackstone"))
+                || (blockName.contains("redstone"))
+                || (blockName.contains("dripstone"))
+                || (blockName.contains("stone"))
+                || (blockName.contains("andesite"))
+                || (blockName.contains("diorite"))
+                || (blockName.contains("granite"))
+                || (blockName.contains("prismarine"))
+                || (blockName.contains("purpur"))
+                || (blockName.contains("deepslate"))
+                || (blockName.contains("tuff"))
+                || (blockName.contains("packed_mud"))
+                || (blockName.contains("quartz"))
+                || (blockName.contains("nether_bricks"))
+                || (blockName.contains("basalt"))
+                || (blockName.contains("coal"))
+                || (blockName.contains("iron"))
+                || (blockName.contains("gold"))
+                || (blockName.contains("emerald"))
+                || (blockName.contains("diamond"))
+                || (blockName.contains("copper"))
+                || (blockName.contains("lapis"))
+                || (blockName.contains("netherite"))
+                || (blockName.contains("obsidian"))
+                || (blockName.contains("amethyst"))
+        )
+        {
+            this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block);
+            //need stone tool
+            if ((blockName.contains("iron"))
+                    || (blockName.contains("lapis"))
+                    || (blockName.contains("copper"))
+            )
+            {
+                this.tag(BlockTags.NEEDS_STONE_TOOL).add(block);
+            }
+            //need iron tool
+            else if ((blockName.contains("gold"))
+                    || (blockName.contains("emerald"))
+                    || (blockName.contains("diamond"))
+            )
+            {
+                this.tag(BlockTags.NEEDS_IRON_TOOL).add(block);
+            }
+            //need diamond tool
+            else if ((blockName.contains("netherite"))
+                    || (blockName.contains("obsidian"))
+            )
+            {
+                this.tag(BlockTags.NEEDS_DIAMOND_TOOL).add(block);
+            }
+        }
+        //mineable with axe
+        else if ((blockName.contains("dark_oak"))
+                || (blockName.contains("oak"))
+                || (blockName.contains("birch"))
+                || (blockName.contains("spruce"))
+                || (blockName.contains("jungle"))
+                || (blockName.contains("acacia"))
+                || (blockName.contains("mangrove"))
+                || (blockName.contains("cherry"))
+                || (blockName.contains("crimson"))
+                || (blockName.contains("warped"))
+                || (blockName.contains("bamboo"))
+        )
+        {
+            this.tag(BlockTags.MINEABLE_WITH_AXE).add(block);
         }
     }
 }
