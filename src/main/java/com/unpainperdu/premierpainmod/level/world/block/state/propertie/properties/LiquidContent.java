d6c7b3e3a25472d0b36c5846b37a2fa3e94abaa9
Ethan Capouillez <ethan.capouillez@hotmail.be>
2025-02-02 21:28:25 +0100

    feat : add new liquid content state

--- src/main/java/com/unpainperdu/premierpainmod/level/world/block/state/propertie/properties/LiquidContent.java
+++ src/main/java/com/unpainperdu/premierpainmod/level/world/block/state/propertie/properties/LiquidContent.java
@@ -8,7 +8,15 @@ public enum LiquidContent implements StringRepresentable
 {
     EMPTY("empty", 0xf1FFFFFF, new Vector3f(1.0f, 1.0f, 1.0f)),
     WATER("water", 0xf13F76E4, new Vector3f(63f/255f, 118f/255f, 228f/255f)),
-    BLOND_BEER("blond_beer", 0xf1faa12d, new Vector3f(250f/255f, 161f/255f, 45f/255f))
+    BLOND_BEER("blond_beer", 0xf1faa12d, new Vector3f(250f/255f, 161f/255f, 45f/255f)),
+    BROWN_BEER("brown_beer", 0xf1935800, new Vector3f(147f/255f, 88f/255f, 0f)),
+    GREEN_BEER("green_beer", 0xf1369244, new Vector3f(54f/255f, 146f/255f, 68f/255f)),
+    RED_BEER("red_beer", 0xf1ce2222, new Vector3f(206f/255f, 34f/255f, 34f/255f)),
+    DARK_RED_BEER("dark_red_beer", 0xf1520c0c, new Vector3f(82f/255f, 12f/255f, 12f/255f)),
+    WHITE_BEER("white_beer", 0xf1e9de95, new Vector3f(233f/255f, 222f/255f, 149f/255f)),
+    BLACK_BEER("black_beer", 0xf12c1a04, new Vector3f(44f/255f, 26f/255f, 04f/255f)),
+    AMBER_BEER("amber_beer", 0xf1fa7700, new Vector3f(250f/255f, 119f/255f, 0)),
+    PURPLE_BEER("purple_beer", 0xf1ac08cc, new Vector3f(172f/255f, 8f/255f, 204f/255f))
     ;
 
     private final String name;
