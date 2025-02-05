f4d2640dbb5ada7290013ba984050ad91d5333df
Ethan Capouillez <ethan.capouillez@hotmail.be>
2025-02-05 15:00:50 +0100

    change : give phisics to foam particle

--- src/main/java/com/unpainperdu/premierpainmod/client/particle/beer_particle/BeerFoamParticle.java
+++ src/main/java/com/unpainperdu/premierpainmod/client/particle/beer_particle/BeerFoamParticle.java
@@ -22,7 +22,7 @@ public class BeerFoamParticle extends TextureSheetParticle
         this.spriteSet = spriteSet;
         this.gravity = 0.0f;
         this.scale(RandomUtil.getRandomPositiveIntInRange(4, randomSource)*0.5f + 2.0f);
-        this.hasPhysics = false;
+        this.hasPhysics = true;
         this.setSpriteFromAge(spriteSet);
         this.friction = 1.0F;
         setParticleSpeed(RandomUtil.getRandomIntInRange(1, randomSource) * speedMultiplier,0,RandomUtil.getRandomIntInRange(1, randomSource) * speedMultiplier);
