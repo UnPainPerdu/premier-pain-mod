9936063d270df2f95f8ba4ca79c6fa7fdb226054
Ethan Capouillez <ethan.capouillez@hotmail.be>
2025-05-21 14:13:50 +0200

    add jungle underground petra main entrance

---
+++ src/main/java/com/unpainperdu/premierpainmod/level/world/worldgen/structure/UndergroundVerticalSurfaceAccessStructure.java
@@ -0,0 +1,112 @@
+package com.unpainperdu.premierpainmod.level.world.worldgen.structure;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.unpainperdu.premierpainmod.util.register.StructureRegister;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Holder;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.ChunkPos;
+import net.minecraft.world.level.chunk.ChunkGenerator;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.levelgen.WorldGenerationContext;
+import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureType;
+import net.minecraft.world.level.levelgen.structure.pools.DimensionPadding;
+import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
+import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
+import net.minecraft.world.level.levelgen.structure.pools.alias.PoolAliasLookup;
+import net.minecraft.world.level.levelgen.structure.structures.JigsawStructure;
+import net.minecraft.world.level.levelgen.structure.templatesystem.LiquidSettings;
+
+import java.util.Optional;
+
+public class UndergroundVerticalSurfaceAccessStructure extends Structure
+{
+    public static final MapCodec<UndergroundVerticalSurfaceAccessStructure> CODEC = RecordCodecBuilder.mapCodec(instance ->
+            instance.group(UndergroundVerticalSurfaceAccessStructure.settingsCodec(instance),
+                    StructureTemplatePool.CODEC.fieldOf("start_pool").forGetter(structure -> structure.startPool),
+                    ResourceLocation.CODEC.optionalFieldOf("start_jigsaw_name").forGetter(structure -> structure.startJigsawName),
+                    Codec.intRange(0, 128).fieldOf("size").forGetter(structure -> structure.size),
+                    HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
+                    Heightmap.Types.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter(structure -> structure.projectStartToHeightmap),
+                    Codec.intRange(1, 512).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
+                    DimensionPadding.CODEC.optionalFieldOf("dimension_padding", JigsawStructure.DEFAULT_DIMENSION_PADDING).forGetter(structure -> structure.dimensionPadding),
+                    LiquidSettings.CODEC.optionalFieldOf("liquid_settings", JigsawStructure.DEFAULT_LIQUID_SETTINGS).forGetter(structure -> structure.liquidSettings)
+            ).apply(instance, UndergroundVerticalSurfaceAccessStructure::new));
+
+    private final Holder<StructureTemplatePool> startPool;
+    private final Optional<ResourceLocation> startJigsawName;
+    private final int size;
+    private final HeightProvider startHeight;
+    private final Optional<Heightmap.Types> projectStartToHeightmap;
+    private final int maxDistanceFromCenter;
+    private final DimensionPadding dimensionPadding;
+    private final LiquidSettings liquidSettings;
+
+    protected UndergroundVerticalSurfaceAccessStructure(StructureSettings config,
+                                                        Holder<StructureTemplatePool> startPool,
+                                                        Optional<ResourceLocation> startJigsawName,
+                                                        int size,
+                                                        HeightProvider startHeight,
+                                                        Optional<Heightmap.Types> projectStartToHeightmap,
+                                                        int maxDistanceFromCenter,
+                                                        DimensionPadding dimensionPadding,
+                                                        LiquidSettings liquidSettings)
+    {
+        //gather param from the json structure
+        super(config);
+        this.startPool = startPool;
+        this.startJigsawName = startJigsawName;
+        this.size = size;
+        this.startHeight = startHeight;
+        this.projectStartToHeightmap = projectStartToHeightmap;
+        this.maxDistanceFromCenter = maxDistanceFromCenter;
+        this.dimensionPadding = dimensionPadding;
+        this.liquidSettings = liquidSettings;
+    }
+
+    private static boolean isSpawnable(GenerationContext context, int posY)
+    {
+        ChunkGenerator chunkGenerator = context.chunkGenerator();
+        boolean flag = true;
+        if (chunkGenerator.getSeaLevel() <= posY)
+        {
+            flag = false;
+        }
+        return flag;
+    }
+
+    @Override
+    protected Optional<GenerationStub> findGenerationPoint(GenerationContext context)
+    {
+        int startY = this.startHeight.sample(context.random(), new WorldGenerationContext(context.chunkGenerator(), context.heightAccessor()));
+        if (!UndergroundVerticalSurfaceAccessStructure.isSpawnable(context,startY))
+        {
+            return Optional.empty();
+        }
+        ChunkPos chunkPos = context.chunkPos();
+        BlockPos blockPos = new BlockPos(chunkPos.getMinBlockX(), startY, chunkPos.getMinBlockZ());
+
+        return JigsawPlacement.addPieces(
+                        context,
+                        this.startPool,
+                        this.startJigsawName,
+                        this.size,
+                        blockPos,
+                        false,
+                        this.projectStartToHeightmap,
+                        this.maxDistanceFromCenter,
+                        PoolAliasLookup.EMPTY,
+                        this.dimensionPadding,
+                        this.liquidSettings);
+    }
+
+    @Override
+    public StructureType<?> type()
+    {
+        return StructureRegister.UNDERGROUND_VERTICAL_SURFACE_ACCESS_STRUCTURE.get();
+    }
+}
--- src/main/java/com/unpainperdu/premierpainmod/util/register/StructureRegister.java
+++ src/main/java/com/unpainperdu/premierpainmod/util/register/StructureRegister.java
@@ -4,6 +4,7 @@ import com.mojang.serialization.MapCodec;
 import com.unpainperdu.premierpainmod.PremierPainMod;
 import com.unpainperdu.premierpainmod.level.world.worldgen.structure.OldGreatFieldStructures;
 import com.unpainperdu.premierpainmod.level.world.worldgen.structure.PremierPainTempleStructures;
+import com.unpainperdu.premierpainmod.level.world.worldgen.structure.UndergroundVerticalSurfaceAccessStructure;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
@@ -26,6 +27,7 @@ public class StructureRegister
 
     public static final DeferredHolder<StructureType<?>, StructureType<PremierPainTempleStructures>> PREMIER_PAIN_TEMPLE_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("premier_pain_temple_structure", () -> explicitStructureTypeTyping(PremierPainTempleStructures.CODEC));
     public static final DeferredHolder<StructureType<?>, StructureType<OldGreatFieldStructures>> OLD_GREAT_FIELD_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("old_great_field_structure", () -> explicitStructureTypeTyping(OldGreatFieldStructures.CODEC));
+    public static final DeferredHolder<StructureType<?>, StructureType<UndergroundVerticalSurfaceAccessStructure>> UNDERGROUND_VERTICAL_SURFACE_ACCESS_STRUCTURE = DEFERRED_REGISTRY_STRUCTURE.register("underground_vertical_surface_access_structure", () -> explicitStructureTypeTyping(UndergroundVerticalSurfaceAccessStructure.CODEC));
 
     private static <T extends Structure> StructureType<T> explicitStructureTypeTyping(MapCodec<T> structureCodec)
     {
