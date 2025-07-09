1b843ff7df5f8f86766454f3a51538d6edf17d12
Ethan Capouillez <ethan.capouillez@hotmail.be>
2025-07-09 13:38:53 +0200

    clean code

--- src/main/java/com/unpainperdu/premierpainmod/client/render/block_entity/CookingPotRender.java
+++ src/main/java/com/unpainperdu/premierpainmod/client/render/block_entity/CookingPotRender.java
@@ -30,16 +30,18 @@ public class CookingPotRender implements BlockEntityRenderer<CookingPotBlockEnti
     @Override
     public void render(CookingPotBlockEntity blockEntity, float partialTick, @NotNull PoseStack poseStack, @NotNull MultiBufferSource buffer, int packedLight, int packedOverlay)
     {
-
-        FluidTank tank = blockEntity.getFluidTank();
-
-        if (!tank.isEmpty())
+        Level level = blockEntity.getLevel();
+        if (level != null)
         {
-            Level level = blockEntity.getLevel();
-            if (level != null)
+            FluidTank tank = blockEntity.getFluidTank();
+            BlockState state = blockEntity.getBlockState();
+            Direction direction = state.getValue(VillagerBrewingStation.FACING);
+            RandomSource rand = level.getRandom();
+            if (!tank.isEmpty())
             {
+
+
                 FluidStack fluid = tank.getFluid();
-                RandomSource rand = level.getRandom();
                 IClientFluidTypeExtensions fluidTypeExtensions = IClientFluidTypeExtensions.of(fluid.getFluid());
                 ResourceLocation stillTexture = fluidTypeExtensions.getStillTexture(fluid);
                 TextureAtlasSprite sprite = Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(stillTexture);
@@ -47,8 +49,7 @@ public class CookingPotRender implements BlockEntityRenderer<CookingPotBlockEnti
                 FluidState fluidState = fluid.getFluid().defaultFluidState();
                 int tintColor = fluidTypeExtensions.getTintColor(fluidState, level, pos);
                 VertexConsumer builder = buffer.getBuffer(RenderType.entityTranslucent(sprite.atlasLocation()));
-                BlockState state = blockEntity.getBlockState();
-                Direction direction = state.getValue(VillagerBrewingStation.FACING);
+
 
                 renderFluid(builder, sprite, poseStack,
                         tintColor, packedLight, packedOverlay,
@@ -56,23 +57,23 @@ public class CookingPotRender implements BlockEntityRenderer<CookingPotBlockEnti
                 );
                 if (state.getValue(BlockStateProperties.LIT))
                 {
-                    double x = pos.getX() + ((RandomUtil.getRandomPositiveIntInRange(6, rand) + 5)/16F);
-                    double y = pos.getY() + ((RandomUtil.getRandomPositiveIntInRange(10, rand) + 6)/16F);
-                    double z = pos.getZ() + ((RandomUtil.getRandomPositiveIntInRange(6, rand) + 5)/16F);
-                    level.addParticle(ParticleTypes.BUBBLE, x ,y ,z,0, 0.5d, 0);
+                    double x = pos.getX() + ((RandomUtil.getRandomPositiveIntInRange(6, rand) + 5) / 16F);
+                    double y = pos.getY() + ((RandomUtil.getRandomPositiveIntInRange(10, rand) + 6) / 16F);
+                    double z = pos.getZ() + ((RandomUtil.getRandomPositiveIntInRange(6, rand) + 5) / 16F);
+                    level.addParticle(ParticleTypes.BUBBLE, x, y, z, 0, 0.5d, 0);
                 }
             }
         }
     }
 
     public static void renderFluid(VertexConsumer builder, TextureAtlasSprite sprite, PoseStack poseStack,
-                                      int tintColor, int packedLight, int packedOverlay, Direction direction
+                                   int tintColor, int packedLight, int packedOverlay, Direction direction
     )
     {
         FluidDisplayRenderHelper.drawFaceRelativelyHorizontally(builder, sprite, poseStack,
                 4.001F, 7.999F, 4.001F,
                 12.001F, 12.001F,
-                tintColor, packedLight,packedOverlay,
+                tintColor, packedLight, packedOverlay,
                 direction
         );
     }
