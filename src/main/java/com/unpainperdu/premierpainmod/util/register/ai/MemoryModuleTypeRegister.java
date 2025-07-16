febfd423d9776ed4e4dfc7d4d5f8eff729c577d6
Ethan Capouillez <ethan.capouillez@hotmail.be>
2025-07-16 20:25:04 +0200

    change register supplier to deferred holder

--- src/main/java/com/unpainperdu/premierpainmod/util/register/ai/MemoryModuleTypeRegister.java
+++ src/main/java/com/unpainperdu/premierpainmod/util/register/ai/MemoryModuleTypeRegister.java
@@ -2,24 +2,27 @@ package com.unpainperdu.premierpainmod.util.register.ai;
 
 import com.mojang.serialization.Codec;
 import com.unpainperdu.premierpainmod.PremierPainMod;
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.ai.memory.MemoryModuleType;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.Optional;
-import java.util.function.Supplier;
 
 public class MemoryModuleTypeRegister
 {
     public static final DeferredRegister<MemoryModuleType<?>> MEMORY_MODULE_TYPE = DeferredRegister.create(Registries.MEMORY_MODULE_TYPE, PremierPainMod.MOD_ID);
 
-    private static <T> Supplier<MemoryModuleType<T>> register(String id)
+    public static final DeferredHolder<MemoryModuleType<?>, MemoryModuleType<BlockPos>> CHOSEN_FIELD = register("chosen_field");
+
+    private static <T> DeferredHolder<MemoryModuleType<?>, MemoryModuleType<T>> register(String id)
     {
         return register(id, Optional.empty());
     }
 
-    private static <T> Supplier<MemoryModuleType<T>> register(String id, Optional<Codec<T>> codec)
+    private static <T> DeferredHolder<MemoryModuleType<?>, MemoryModuleType<T>> register(String id, Optional<Codec<T>> codec)
     {
         return MEMORY_MODULE_TYPE.register(id, () -> new MemoryModuleType<>(codec));
     }
--- src/main/java/com/unpainperdu/premierpainmod/util/register/ai/SensorsTypeRegister.java
+++ src/main/java/com/unpainperdu/premierpainmod/util/register/ai/SensorsTypeRegister.java
@@ -5,6 +5,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.ai.sensing.Sensor;
 import net.minecraft.world.entity.ai.sensing.SensorType;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
@@ -13,7 +14,9 @@ public class SensorsTypeRegister
 {
     public static final DeferredRegister<SensorType<?>> SENSOR_TYPE = DeferredRegister.create(Registries.SENSOR_TYPE, PremierPainMod.MOD_ID);
 
-    private static <T extends Sensor<?>> Supplier<SensorType<T>> register(String id, Supplier<T> sensor)
+    //public static final DeferredHolder<SensorType<?>, SensorType<X>>
+
+    private static <T extends Sensor<?>> DeferredHolder<SensorType<?>, SensorType<T>> register(String id, Supplier<T> sensor)
     {
         return SENSOR_TYPE.register(id, () -> new SensorType<>(sensor));
     }
