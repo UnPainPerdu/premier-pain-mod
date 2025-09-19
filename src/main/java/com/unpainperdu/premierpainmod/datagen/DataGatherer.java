64f36eeae945a54402b79d0627ac6d157de80fa7
Ethan Capouillez <ethan.capouillez@hotmail.be>
2025-09-19 17:58:06 +0200

    rework a bit data generation

--- src/main/java/com/unpainperdu/premierpainmod/datagen/DataGatherer.java
+++ src/main/java/com/unpainperdu/premierpainmod/datagen/DataGatherer.java
@@ -10,10 +10,7 @@ import com.unpainperdu.premierpainmod.datagen.data.*;
 import com.unpainperdu.premierpainmod.datagen.data.data_pack_registries.ModDataPackProvider;
 import com.unpainperdu.premierpainmod.datagen.data.datamap.ModDataMap;
 import com.unpainperdu.premierpainmod.datagen.data.loot_table.ModLootTableProvider;
-import com.unpainperdu.premierpainmod.datagen.data.tag.ModBiomeTagProvider;
-import com.unpainperdu.premierpainmod.datagen.data.tag.ModBlockTagProvider;
-import com.unpainperdu.premierpainmod.datagen.data.tag.ModFluidTag;
-import com.unpainperdu.premierpainmod.datagen.data.tag.ModItemTagProvider;
+import com.unpainperdu.premierpainmod.datagen.data.tag.*;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
@@ -31,19 +28,15 @@ public class DataGatherer
         ExistingFileHelper fileHelper = event.getExistingFileHelper();
         CompletableFuture<HolderLookup.Provider> lookupProvider = ModDataPackProvider.onGatherData(event);
 
-        BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, fileHelper);
-        new ModLanguageProvider(event, generator, packOutput);
+        ModLanguageProvider.spliter(event, generator, packOutput);
+        ModTagSpliter.spliter(event.includeServer(), generator, packOutput, fileHelper, lookupProvider);
         generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
         generator.addProvider(event.includeServer(), new ModLootTableProvider(packOutput, lookupProvider));
-        generator.addProvider(event.includeServer(), blockTagsProvider);
-        generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput,lookupProvider, blockTagsProvider.contentsGetter(), fileHelper));
-        generator.addProvider(event.includeServer(),new ModBiomeTagProvider(packOutput, lookupProvider, fileHelper));
         generator.addProvider(event.includeServer(),new ModBlockStateProvider(packOutput, fileHelper));
         generator.addProvider(event.includeServer(),new ModItemStateProvider(packOutput, fileHelper));
         generator.addProvider(event.includeServer(), new ModSoundProvider(packOutput,fileHelper));
         generator.addProvider(event.includeServer(), new ModGlobalLootModifierProvider(packOutput, lookupProvider));
         generator.addProvider(event.includeServer(), new ModDataMap(packOutput, lookupProvider));
-        generator.addProvider(event.includeServer(), new ModFluidTag(packOutput, lookupProvider, fileHelper));
         generator.addProvider(event.includeServer(), new ModAdvancementProvider(packOutput, lookupProvider, fileHelper));
         generator.addProvider(event.includeClient(), new ModParticleDescriptionProvider(packOutput, fileHelper));
     }
