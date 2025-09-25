6abd78537849d7e3f07bd50247a7245bc024cfde
Ethan Capouillez <ethan.capouillez@hotmail.be>
2025-09-25 16:22:19 +0200

    successfully generate all data for forest premier pain temple

--- src/main/java/com/unpainperdu/premierpainmod/datagen/DataGatherer.java
+++ src/main/java/com/unpainperdu/premierpainmod/datagen/DataGatherer.java
@@ -15,6 +15,7 @@ import com.unpainperdu.premierpainmod.datagen.data.level.world.worldgen.biome.Mo
 import com.unpainperdu.premierpainmod.datagen.data.level.world.worldgen.biome.feature.features.ModFeatureUtil;
 import com.unpainperdu.premierpainmod.datagen.data.level.world.worldgen.biome.feature.placement.ModPlacementUtil;
 import com.unpainperdu.premierpainmod.datagen.data.level.world.worldgen.structure.ModStructure;
+import com.unpainperdu.premierpainmod.datagen.data.level.world.worldgen.structure.ModStructureSet;
 import com.unpainperdu.premierpainmod.datagen.data.level.world.worldgen.structure.ModStructureTemplatePool;
 import com.unpainperdu.premierpainmod.datagen.data.loot_table.ModLootTableProvider;
 import com.unpainperdu.premierpainmod.datagen.data.tag.ModTagSpliter;
@@ -68,6 +69,7 @@ public class DataGatherer
                                 .add(Registries.CONFIGURED_FEATURE, ModFeatureUtil::bootstrap)
                                 .add(Registries.STRUCTURE, ModStructure::boostrap)
                                 .add(Registries.TEMPLATE_POOL, ModStructureTemplatePool::boostrap)
+                                .add(Registries.STRUCTURE_SET, ModStructureSet::boostrap)
                         ,
                         Set.of(PremierPainMod.MOD_ID))
         ).getRegistryProvider();
---
+++ src/main/java/com/unpainperdu/premierpainmod/datagen/data/level/world/worldgen/structure/ModStructureSet.java
@@ -0,0 +1,37 @@
+package com.unpainperdu.premierpainmod.datagen.data.level.world.worldgen.structure;
+
+import com.unpainperdu.premierpainmod.util.tool_kit.ResourceUtil;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructureSets;
+import net.minecraft.world.level.levelgen.structure.BuiltinStructures;
+import net.minecraft.world.level.levelgen.structure.Structure;
+import net.minecraft.world.level.levelgen.structure.StructureSet;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadStructurePlacement;
+import net.minecraft.world.level.levelgen.structure.placement.RandomSpreadType;
+
+public class ModStructureSet
+{
+    public static final ResourceKey<StructureSet> FOREST_PREMIER_PAIN_TEMPLE_SET = register("forest_premier_pain_temple_set");
+
+    private static ResourceKey<StructureSet> register(String path)
+    {
+        return ResourceKey.create(Registries.STRUCTURE_SET, ResourceUtil.createResourceLocation(path));
+    }
+
+    public static void boostrap(BootstrapContext<StructureSet> context)
+    {
+        HolderGetter<Structure> structureHoldergetter = context.lookup(Registries.STRUCTURE);
+        HolderGetter<Biome> biomeHoldergetter = context.lookup(Registries.BIOME);
+
+        context.register(
+                FOREST_PREMIER_PAIN_TEMPLE_SET,
+                new StructureSet(
+                        structureHoldergetter.getOrThrow(ModStructure.FOREST_PREMIER_PAIN_TEMPLE), new RandomSpreadStructurePlacement(50, 45, RandomSpreadType.LINEAR, 1642136474)
+                )
+        );
+    }
+}
