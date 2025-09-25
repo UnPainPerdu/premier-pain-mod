795e9a170e603a61709ec0219c89dfa632b90daa
Ethan Capouillez <ethan.capouillez@hotmail.be>
2025-09-25 15:36:14 +0200

    generate 1st pool forest premier pain temple

--- src/main/java/com/unpainperdu/premierpainmod/datagen/data/level/world/worldgen/structure/ModStructureTemplatePool.java
+++ src/main/java/com/unpainperdu/premierpainmod/datagen/data/level/world/worldgen/structure/ModStructureTemplatePool.java
@@ -1,13 +1,23 @@
 package com.unpainperdu.premierpainmod.datagen.data.level.world.worldgen.structure;
 
+import com.google.common.collect.ImmutableList;
+import com.mojang.datafixers.util.Pair;
 import com.unpainperdu.premierpainmod.util.tool_kit.ResourceUtil;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.Pools;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.level.levelgen.structure.pools.StructurePoolElement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 
 public class ModStructureTemplatePool
 {
+    /*
+     *   If pool = 1 element, in this class.
+     *   Check PlainVillagePools.java for example.
+     **/
     public static final ResourceKey<StructureTemplatePool> FOREST_PREMIER_PAIN_TEMPLE_POOL = damageTypesRegister("forest_premier_pain_temple_pool");
 
     private static ResourceKey<StructureTemplatePool> damageTypesRegister(String path)
@@ -17,6 +27,18 @@ public class ModStructureTemplatePool
 
     public static void boostrap(BootstrapContext<StructureTemplatePool> context)
     {
+        HolderGetter<StructureTemplatePool> vanillaTemplatePoolHolderGetter = context.lookup(Registries.TEMPLATE_POOL);
+        Holder<StructureTemplatePool> EmptyStructureTemplatePool = vanillaTemplatePoolHolderGetter.getOrThrow(Pools.EMPTY);
 
+        context.register(
+                FOREST_PREMIER_PAIN_TEMPLE_POOL,
+                new StructureTemplatePool(
+                        EmptyStructureTemplatePool,
+                        ImmutableList.of(
+                                Pair.of(StructurePoolElement.single("premierpainmod:premier_pain_temple/forest_premier_pain_temple_structure"), 1)
+                        ),
+                        StructureTemplatePool.Projection.RIGID
+                )
+        );
     }
 }
